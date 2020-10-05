266785d0d580e92f7d6109ea2237ae19c91c4d94
MartinLym <38201711+MartinLym@users.noreply.github.com>
2020-10-06 10:14:26 +1100

    real robot main control

--- main.m
+++ main.m
@@ -1,28 +1,35 @@
 clc
-clf
-clear all
+clear
+robot = HANSCUTE();
+robot.initROSConnection();
 
-workspace = [-1 1 -1 1 -1 1];
-scale = 0.25;
-qStart = deg2rad([-81,0,-143,30.9,47.4,49,0]);
+[armControllerPub, armControllerMsg, clawControllerPub, clawControllerMsg] = robot.setupHansPublisher();
+%q0 = [-1.1382 0.3636 -0.8299 -0.5062 -0.9986 1.1658 0];
+%q = [0 0 0 0 0 0 0];
+qStart = [-0.1427 0.9081 -0.0430 0.7179 -0.0828 1.4404 -0.0077];
+qGrab = [0.0227 1.4494 -0.0276 0.7333 -0.0874 -0.6169 -0.1546];
+qCurrent = robot.obtainCurrentJointStates
+qCurrent =qCurrent(1:7);
 
-r = RobotControl();
-r.robot.model.plot(qStart,'workspace',workspace,'scale',scale,'nowrist');
-axis equal;
-r.SimulateRobot();
+robot.model.teach(qCurrent);
+hold on
+show = false; % show = true to display the object for object collision size
+diameter = 0.25;
+height = 0.11;
+% section below for defining objects in the workspace
+obj(1) = Environment(0.2, -0.2 ,0, 'plate3.ply', show, diameter, height);
+%%
+qCurrent = robot.obtainCurrentJointStates
+qCurrent =qCurrent(1:7);
+startPose = robot.model.fkine(qCurrent);
+endPose = robot.model.fkine(qStart);
+[qMatrix, sendQMatrix, velMatrix, trMatrix, poseMatrix, coordMatrix, positionError, angleError, m] = robot.obtainMotionMatrices(startPose, endPose, 1500, obj);
+robot.moveArm(sendQMatrix, armControllerPub, armControllerMsg);
 
 %%
-clc
-clf
-clear all
-
-workspace = [-1 1 -1 1 -1 1];
-scale = 0.25;
-qStart = deg2rad([-81,0,-143,30.9,47.4,49,0]);
-r = RobotControl();
-
-r.robot.model.plot(qStart,'workspace',workspace,'scale',scale,'nowrist');
-hold on;
-r.robot.model.teach();
-
-
+qCurrent = robot.obtainCurrentJointStates
+qCurrent =qCurrent(1:7);
+startPose = robot.model.fkine(qCurrent);
+endPose = robot.model.fkine(qGrab);
+[qMatrix, sendQMatrix, velMatrix, trMatrix, poseMatrix, coordMatrix, positionError, angleError, m] = robot.obtainMotionMatrices(startPose, endPose, 1500, obj);
+robot.moveArm(sendQMatrix, armControllerPub, armControllerMsg);
